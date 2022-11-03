68d0d21a4be13e16f83dfe619cfe054153a290ec
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-11-03 14:10:21 +0300

    commit tree 714ddc22266eee58f88c78a600df9ae226d5420d

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def characters
+    Character.where(show_id: self.id)
+  end
+
+  def network
+    Network.find(self.network_id)
+  end
+
+  def actors_list
+    self.actors.collect do |actor|
+      actor.full_name
+    end
+  end
 end
