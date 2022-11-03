4f53685eddba6e724d233d17d0b6145ec0e8fd20
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-11-03 14:14:06 +0300

    not complete

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,24 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def characters
+    Character.where(actor_id: self.id)
+  end
+
+  def shows
+    Show.where(actor_id: self.id)
+  end
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
   
+
 end
